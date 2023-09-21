fffd35d2e3e934ad00ad4bc28682ba9243ca3016
antorahad <antorahad6@gmail.com>
2023-09-21 22:54:12 +0600

    career hub start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <center className='mt-[200px]'>
+      <h1 className='text-5xl font-bold text-primary mb-[30px]'>All are working 100%</h1>
+     <button className="btn btn-active btn-primary">Primary</button>
+     </center>
+    </>
+  )
+}
+
+export default App
